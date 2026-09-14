9f63d29a7a7333cacf16f6ec02afff107d8260cf
Pratik Nikumbe <80843204+pratik80@users.noreply.github.com>
2026-09-14 13:25:55 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,16 @@
+function Show() {
+	document.getElementById('userlogin').style.display="none";
+	document.getElementById('usersignup').style.display="block";
+}
+function Hide() {
+	document.getElementById('userlogin').style.display="block";
+	document.getElementById('usersignup').style.display="none";
+}
+function Show1() {
+	document.getElementById('adminlogin').style.display="none";
+	document.getElementById('adminsignup').style.display="block";
+}
+function Hide1() {
+	document.getElementById('adminlogin').style.display="block";
+	document.getElementById('adminsignup').style.display="none";
+}
